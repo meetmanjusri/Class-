17adc6715cc48858302b827e2bd010dfdb279c63
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-01-30 13:07:46 -0600

    Print name checkin.

---
+++ Class1/Print Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Name
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
