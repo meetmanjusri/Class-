4ed31a22f4c77bdee67affb72454a8bf7133a31a
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-02-01 14:50:06 -0600

    Final program

--- Chapter3/Chapter3/Program.cs
+++ Chapter3/Chapter3/Program.cs
@@ -10,7 +10,20 @@ namespace Chapter3
     {
         static void Main(string[] args)
         {
-        
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
+            Console.Read();
+
         }
     }
 }
