beee3483f79c13bd66e331f9c7d7a824a92329e7
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-02-04 23:32:15 -0600

    FibonacciSeries Checkin.

---
+++ Homework1/FibonacciSeries/FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 0;
+            int y = 1;
+            int z;
+            Console.WriteLine("Enter Starting Number: ");
+            int StartingNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Ending Number: ");
+            int EndingNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci series is : ");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            for (int i = StartingNumber; i <EndingNumber; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+               
+                if (z>= StartingNumber && z > EndingNumber)
+                {
+                    break;
+                }
+                Console.WriteLine(z);
+            }
+
+        }
+    }
+}
+
