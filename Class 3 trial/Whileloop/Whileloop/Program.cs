2447b118e7c8add220717999a2f0cf860a847c1f
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-02-06 12:21:06 -0600

    Whileloop checkin

---
+++ Class 3 trial/Whileloop/Whileloop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int result =0;
+            string x="Y";
+            while (x.ToUpper() == "Y")
+            {
+                Console.Write("Enter a number:");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Do you want to continue (Y/N) ?");
+                x = Console.ReadLine();
+                result = result + n;
+
+            }
+            Console.WriteLine("Total sum:" + result);
+            
+        }
+    }
+}
