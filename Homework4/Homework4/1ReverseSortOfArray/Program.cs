e86043ced337bbadb4a59e023586981d4e876e68
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-02-18 13:53:26 -0600

    Homework4 checkin

---
+++ Homework4/Homework4/1ReverseSortOfArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1ReverseSortOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the length of the Array: ");
+            var arraySize = Int32.Parse(Console.ReadLine());
+            int[] userArray = new int[arraySize];
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                Console.Write("Enter the value at index 0:");
+                userArray[i] = Int32.Parse(Console.ReadLine());
+            }
+            Array.Sort(userArray);
+            Console.WriteLine("Reverse sorted array:");
+            for (int i = userArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(userArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
