4323857ab7a0966d1704e277624a8dc6ff39bc13
meetmanjusri@gmail.com <meetmanjusri@gmail.com>
2016-02-18 19:15:52 -0600

    Homework 4 Final Checkin

---
+++ Homework4/Homework4/2RetrieveNamefromString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2RetrieveNamefromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleString;
+            string inputname = "";
+            Console.Write("What is your name? : " + "My name is ");
+            inputname = Console.ReadLine();
+            sampleString = inputname.Substring(0,inputname.Length);
+            Console.WriteLine("Hi " + sampleString + "." + "How are you doing today?");
+           
+        }
+    }
+}
---
+++ Homework4/Homework4/3.PalindromeOrNot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.PalindromeOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter a string (Q:Exit): ");
+                string inputname = Console.ReadLine();
+
+                if (inputname != null && inputname != "Q")
+                {
+                    string strToUpper = inputname.ToUpper();
+                    char[] toReverse = strToUpper.ToCharArray();
+                    Array.Reverse(toReverse);
+                    String strReverse = new String(toReverse);
+                    if (strToUpper == strReverse)
+                    {
+                        Console.WriteLine(inputname + " is a palindrome");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(inputname + " is not a palindrome");
+                    }
+                }
+                if (inputname == "Q")
+                {
+                    Console.WriteLine("Thank you Bye Bye");
+                    break;
+                }
+            }
+        }
+    }
+}
+   
+  
+
